77e370d52c3b307fb7cc269d8ba7682f34cd02a6
yuya024 <yuyaaanissy@gmail.com>
2023-12-21 17:08:37 +0900

    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action do
+    I18n.locale = :ja
+  end
 end
