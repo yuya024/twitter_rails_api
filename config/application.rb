09b1be4d260a0142fd4bc3bf6908503a7edcd08e
yuya024 <yuyaaanissy@gmail.com>
2023-12-27 12:29:27 +0900

    ログイン機能の実装

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,10 @@ module Myapp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: false,
