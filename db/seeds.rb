a24238d9ac16e669050b0217f4be3ce29b22377b
yuya024 <yuyaaanissy@gmail.com>
2024-01-14 21:55:45 +0900

    seedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+content = ['今日は暑い！', '今日は寒い！', '今日は涼しいな！', '今日は暖かいな！', "もくもく！\n今日も頑張ろう!"]
+
+10.times do |n|
+  user = User.find_or_initialize_by(email: "user#{n + 1}@example.com")
+  user.name = "user#{n + 1}"
+  user.password = "user#{n + 1}pass"
+  user.skip_confirmation!
+  user.save!
+  user.profile_image.attach(io: Rails.root.join('app/assets/images/default_profile.jpg').open,
+                            filename: 'default_profile.jpg')
+  user.header_image.attach(io: Rails.root.join('app/assets/images/default_header.jpg').open,
+                           filename: 'default_header.jpg')
+
+  user.tweets.create!(content: content.sample)
+  user.tweets.create!(content: content.sample)
+end
