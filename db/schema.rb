c74de21807b05faa310a652eb0d646e828a58ab7
yuya024 <yuyaaanissy@gmail.com>
2024-01-20 16:20:26 +0900

    ユーザーモデルにプロフィール項目のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_30_105149) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_083014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_30_105149) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "introduction", limit: 160
+    t.string "location", limit: 30
+    t.string "website", limit: 100
+    t.datetime "birthdate"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
