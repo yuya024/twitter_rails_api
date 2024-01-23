1d23c63101cf63002a05548e8f7273326d24481d
yuya024 <yuyaaanissy@gmail.com>
2024-01-23 11:16:41 +0900

    コメント数を返す関数の実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,9 +4,14 @@ class Tweet < ApplicationRecord
   include Rails.application.routes.url_helpers
 
   belongs_to :user
+  has_many :comments, dependent: :destroy
   has_one_attached :image
 
   def image_url
     image.attached? ? url_for(image) : nil
+  end
+
+  def comment_count
+    comments.count
   end
 end
