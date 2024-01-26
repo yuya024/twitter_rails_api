4a6ac348d463c99d4e17bbb44b0f03272eb3160f
yuya024 <yuyaaanissy@gmail.com>
2024-01-26 17:54:42 +0900

    ログインユーザをグローバルに宣言し、rubocopの設定を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action do
     I18n.locale = :ja
+    $current_api_v1_user = current_api_v1_user
   end
 end
