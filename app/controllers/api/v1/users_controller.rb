8c30a48c29efa98f59bdb7954c315c433accfc91
yuya024 <yuyaaanissy@gmail.com>
2024-01-29 12:21:05 +0900

    対象ユーザーをフォローしているかのjsonを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,15 +7,16 @@ module Api
 
       def show
         user = User.find(params[:id])
+        followed = current_api_v1_user.follower.exists?(followed_id: params[:id])
         if params[:is_comment].present?
           comments = user.comments.page(params[:page] || 1).per(10).order(created_at: :desc)
           render json: { user: user.as_json(methods: %i[profile_image_url header_image_url]),
-                         comments:, pagination: resources_with_pagination(comments) }
+                         comments:, followed:, pagination: resources_with_pagination(comments) }
         else
           tweets = user.tweets.page(params[:page] || 1).per(10).order(created_at: :desc)
           render json: { user: user.as_json(methods: %i[profile_image_url header_image_url]),
                          tweets: tweets.as_json(methods: %i[image_url comment_count]),
-                         pagination: resources_with_pagination(tweets) }
+                         followed:, pagination: resources_with_pagination(tweets) }
         end
       end
     end
